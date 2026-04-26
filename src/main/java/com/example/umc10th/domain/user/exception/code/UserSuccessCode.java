9f8aa1b44dc764472eb60fe9d6573fd86b877cf1
daeunkim701 <lionboot2025@gmail.com>
2026-04-26 21:48:29 +0900

    [Feat(단/김다은)] user 도메인 controller, dto

--- src/main/java/com/example/umc10th/domain/user/exception/code/UserSuccessCode.java
+++ src/main/java/com/example/umc10th/domain/user/exception/code/UserSuccessCode.java
@@ -1,4 +1,18 @@
 package com.example.umc10th.domain.user.exception.code;
 
-public class UserSuccessCode {
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserSuccessCode implements BaseSuccessCode {
+
+    USER_INFO_SUCCESS(HttpStatus.OK, "USER2001", "유저 정보 조회 성공"),
+    USER_UPDATE_SUCCESS(HttpStatus.OK, "USER2002", "유저 정보 수정 성공");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
