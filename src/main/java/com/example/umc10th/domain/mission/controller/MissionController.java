108cec93a827174b547ebade592c363f110d33dd
daeunkim701 <lionboot2025@gmail.com>
2026-05-06 16:41:42 +0900

    [Feat(단/김다은) 구현한 MissionService와 controller 연결(가짜데이터 삭제)

--- src/main/java/com/example/umc10th/domain/mission/controller/MissionController.java
+++ src/main/java/com/example/umc10th/domain/mission/controller/MissionController.java
@@ -2,13 +2,18 @@ package com.example.umc10th.domain.mission.controller;
 
 import com.example.umc10th.domain.mission.dto.MissionResDTO;
 import com.example.umc10th.domain.mission.exception.code.MissionSuccessCode;
+import com.example.umc10th.domain.mission.service.MissionService;
 import com.example.umc10th.global.apiPayload.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/missions")
 public class MissionController {
+
+    private final MissionService missionService;
 
     @GetMapping("/me")
     public ApiResponse<MissionResDTO.MissionList> getMyMissions(
@@ -17,31 +22,11 @@ public class MissionController {
             @RequestParam(required = false) String status
     ) {
 
-        String missionStatus = (status != null) ? status : "ALL";
-
-        // 일단 가짜 데이터 넣기
-        List<MissionResDTO.MissionItem> missions = List.of(
-                MissionResDTO.MissionItem.builder()
-                        .missionId(1L)
-                        .title("탕화쿵푸")
-                        .content("탕화쿵푸 5회 이상 방문 시 포인트 500점")
-                        .reward(500)
-                        .status(missionStatus)
-                        .build()
-        );
-
-        MissionResDTO.MissionList response =
-                MissionResDTO.MissionList.builder()
-                        .missions(missions)
-                        .page(page)
-                        .size(size)
-                        .totalPages(1)
-                        .totalElements(1)
-                        .build();
+        Long userId = 1L; // 임시
 
         return ApiResponse.onSuccess(
                 MissionSuccessCode.MISSION_LIST_SUCCESS,
-                response
+                missionService.getMyMissions(userId, page, size, status)
         );
     }
 
@@ -50,19 +35,11 @@ public class MissionController {
             @PathVariable Long missionId
     ) {
 
-        // 일단 가짜 데이터 넣기
-        MissionResDTO.MissionItem mission =
-                MissionResDTO.MissionItem.builder()
-                        .missionId(missionId)
-                        .title("탕화쿵푸")
-                        .content("탕화쿵푸 5회 이상 방문 시 포인트 500점")
-                        .reward(500)
-                        .status("COMPLETE")
-                        .build();
+        Long userId = 1L;
 
         return ApiResponse.onSuccess(
                 MissionSuccessCode.MISSION_LIST_SUCCESS,
-                mission
+                missionService.getMissionDetail(userId, missionId)
         );
     }
 }
