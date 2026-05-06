c90acec3f394ef18adeb31991cf1b34829917733
daeunkim701 <lionboot2025@gmail.com>
2026-05-06 13:30:42 +0900

    [Feat(단/김다은)] 회원 테이블 entity & 매핑

--- src/main/java/com/example/umc10th/domain/user/entity/Inquiry.java
+++ src/main/java/com/example/umc10th/domain/user/entity/Inquiry.java
@@ -1,4 +1,31 @@
 package com.example.umc10th.domain.user.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "inquiry")
 public class Inquiry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inquiry_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "inquiry_content", nullable = false)
+    private String content;
+
+    @Column(name = "picture_url")
+    private String pictureUrl;
 }
--- src/main/java/com/example/umc10th/domain/user/entity/mapping/UserFood.java
+++ src/main/java/com/example/umc10th/domain/user/entity/mapping/UserFood.java
@@ -1,4 +1,31 @@
 package com.example.umc10th.domain.user.entity.mapping;
 
+import com.example.umc10th.domain.user.entity.Food;
+import com.example.umc10th.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "like_food")
 public class UserFood {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_food_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_id", nullable = false)
+    private Food food;
 }
