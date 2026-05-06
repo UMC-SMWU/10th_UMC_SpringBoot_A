a383b9bb77f7236edd73b652a5b4c5daedf18106
daeunkim701 <lionboot2025@gmail.com>
2026-05-06 13:45:12 +0900

    [Feat(단/김다은)] mission 도메인 entity & 매핑

--- src/main/java/com/example/umc10th/domain/mission/entity/Mission.java
+++ src/main/java/com/example/umc10th/domain/mission/entity/Mission.java
@@ -1,4 +1,42 @@
 package com.example.umc10th.domain.mission.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "mission")
 public class Mission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mission_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @Column(name = "left_date", nullable = false)
+    private Integer leftDate;
+
+    @Column(name = "mission_content", nullable = false, length = 100)
+    private String content;
+
+    @Column(name = "point", nullable = false)
+    private Integer point;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 }
--- src/main/java/com/example/umc10th/domain/mission/entity/Restaurant.java
+++ src/main/java/com/example/umc10th/domain/mission/entity/Restaurant.java
@@ -1,4 +1,30 @@
 package com.example.umc10th.domain.mission.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "restaurant")
 public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "restaurant_id")
+    private Long id;
+
+    @Column(name = "restaurant_name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "star_rating", nullable = false)
+    private Float starRating;
+
+    @Column(name = "address", nullable = false, length = 200)
+    private String address;
+
+    @Column(name = "restaurant_type", nullable = false, length = 50)
+    private String type;
 }
