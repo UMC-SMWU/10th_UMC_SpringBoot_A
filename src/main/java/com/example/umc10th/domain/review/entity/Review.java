6e3567ee6ba88812a642235db067cda938f43392
daeunkim701 <lionboot2025@gmail.com>
2026-05-06 13:40:22 +0900

    [Feat(단/김다은)] review 도메인 entity & 매핑

---
+++ src/main/java/com/example/umc10th/domain/review/entity/Review.java
@@ -0,0 +1,47 @@
+package com.example.umc10th.domain.review.entity;
+
+import com.example.umc10th.domain.mission.entity.Restaurant;
+import com.example.umc10th.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    // User와 N:1 관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @Column(name = "review_content", nullable = false)
+    private String content;
+
+    @Column(name = "star_rating", nullable = false)
+    private Float starRating;
+
+    @Column(name = "picture_url")
+    private String pictureUrl;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
