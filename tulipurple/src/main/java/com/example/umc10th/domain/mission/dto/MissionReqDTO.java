4a94bb0d675f7e66dd8a7cc618473f5048b9f25e
Tulipurple <164765964+Tulipurple@users.noreply.github.com>
2026-05-11 17:17:09 +0900

    [Feat(이브/곽해림)] 검증 어노테이션 추가

--- tulipurple/src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
+++ tulipurple/src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
@@ -1,9 +1,24 @@
 package com.example.umc10th.domain.mission.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+
 public class MissionReqDTO {
 
     // 미션 성공 누르기
     public record CompleteStatus(
-            Boolean isComplete
+        @NotNull(message = "변경할 상태는 필수입니다.")
+        Boolean isComplete
     ){}
+
+    // 가게 미션 생성
+    public record CreateMission(
+        @NotNull(message="마감기한은 필수입니다.")
+        LocalDate deadLine,
+        @NotNull(message="미션 성공 포인트는 필수입니다.")
+        Integer point,
+        @NotBlank(message="조건은 빈칸일 수 없습니다.")
+        String conditional
+    ) {}
 }
