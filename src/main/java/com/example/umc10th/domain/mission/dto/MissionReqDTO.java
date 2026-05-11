27969b57b81950efead051711c8dafdf936d2538
daeunkim701 <lionboot2025@gmail.com>
2026-05-11 23:33:18 +0900

    [Fix(단/김다은)] 미션 요청 DTO에 userId 추가

--- src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
+++ src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
@@ -3,6 +3,7 @@ package com.example.umc10th.domain.mission.dto;
 public class MissionReqDTO {
 
     public record GetMyMissions(
+            Long userId,
             Integer page,
             Integer size,
             String status
