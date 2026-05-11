07f92a710441f889cab4a12b4a3e4e96536e1450
daeunkim701 <lionboot2025@gmail.com>
2026-05-11 23:02:51 +0900

    [Feat(단/김다은)] 리뷰 조회용 & 리스트 응답 DTO 추가

--- src/main/java/com/example/umc10th/domain/review/dto/ReviewResDTO.java
+++ src/main/java/com/example/umc10th/domain/review/dto/ReviewResDTO.java
@@ -1,6 +1,9 @@
 package com.example.umc10th.domain.review.dto;
 
+import com.example.umc10th.domain.review.entity.Review;
 import lombok.Builder;
+
+import java.util.List;
 
 public class ReviewResDTO {
 
@@ -10,6 +13,22 @@ public class ReviewResDTO {
             String title,
             int star,
             String content
+    ) {}
+
+    @Builder
+    public record ReviewItem(
+            Long reviewId,
+            String restaurantName,
+            Float star,
+            String content,
+            String createdAt
+    ) {}
+
+    @Builder
+    public record ReviewList(
+            List<ReviewItem> review,
+            Long nextCursor,
+            boolean hasNext
     ) {}
 
 }
