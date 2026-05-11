4b080e618404eef8fd829a3cc8922d7a729638ce
daeunkim701 <lionboot2025@gmail.com>
2026-05-11 23:06:35 +0900

    [Feat(단/김다은)] controller에 내 리뷰 조회 로직 추가

--- src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
@@ -3,12 +3,17 @@ package com.example.umc10th.domain.review.controller;
 import com.example.umc10th.domain.review.dto.ReviewReqDTO;
 import com.example.umc10th.domain.review.dto.ReviewResDTO;
 import com.example.umc10th.domain.review.exception.code.ReviewSuccessCode;
+import com.example.umc10th.domain.review.service.ReviewService;
 import com.example.umc10th.global.apiPayload.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/reviews")
 public class ReviewController {
+
+    private final ReviewService reviewService;
 
     @PostMapping
     public ApiResponse<ReviewResDTO.CreateReview> createReview(
@@ -24,6 +29,18 @@ public class ReviewController {
         return ApiResponse.onSuccess(
                 ReviewSuccessCode.REVIEW_CREATED,
                 response
+        );
+    }
+
+    @GetMapping("/me")
+    public ApiResponse<ReviewResDTO.ReviewList> getMyReviews(
+            @RequestParam Long userId,
+            @RequestParam(required = false) Long cursor,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sort
+    ) {
+        return ApiResponse.onSuccess(
+                ReviewSuccessCode.REVIEW_LIST_SUCCESS, reviewService.getMyReviews(userId, cursor, size, sort)
         );
     }
 }
