68dc2445fba67fc38df7225b7d50ae89f1c05fa4
daeunkim701 <lionboot2025@gmail.com>
2026-05-16 21:43:41 +0900

    [Feat(단/김다은)] Security exception 작성

--- src/main/java/com/example/umc10th/global/security/entity/AuthUser.java
+++ src/main/java/com/example/umc10th/global/security/entity/AuthUser.java
@@ -1,13 +1,21 @@
 package com.example.umc10th.global.security.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
+import com.example.umc10th.domain.user.entity.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-public class AuthMember implements UserDetails {
+@Getter
+@RequiredArgsConstructor
+public class AuthUser implements UserDetails {
+
+    private final User user;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
@@ -15,11 +23,11 @@ public class AuthMember implements UserDetails {
 
     @Override
     public @Nullable String getPassword() {
-        return "";
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return user.getEmail();
     }
 }
