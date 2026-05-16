f65839183b98219dd938c9bc99e4a35f471a32eb
daeunkim701 <lionboot2025@gmail.com>
2026-05-16 21:46:26 +0900

    [Feat(단/김다은)] Security 에러 응답 형태 통일 및 common 코드 util 분리

--- src/main/java/com/example/umc10th/global/security/exception/CustomAccessDenied.java
+++ src/main/java/com/example/umc10th/global/security/exception/CustomAccessDenied.java
@@ -1,4 +1,30 @@
 package com.example.umc10th.global.security.exception;
 
-public class CustomAccessDenied {
+import com.example.umc10th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc10th.global.apiPayload.code.GeneralErrorCode;
+import com.example.umc10th.global.security.util.SecurityResponseUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class CustomAccessDenied
+        implements AccessDeniedHandler {
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws IOException {
+
+        BaseErrorCode code = GeneralErrorCode.FORBIDDEN;
+
+        SecurityResponseUtil.writeErrorResponse(
+                response,
+                code
+        );
+    }
 }
