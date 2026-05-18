7deea817e2336f4bb0f5b40ab65b85a0fc86d152
daeunkim701 <lionboot2025@gmail.com>
2026-05-18 13:15:26 +0900

    [Feat(단/김다은)] AuthController 회원가입 API 구현

--- src/main/java/com/example/umc10th/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/umc10th/domain/auth/controller/AuthController.java
@@ -1,4 +1,30 @@
 package com.example.umc10th.domain.auth.controller;
 
+import com.example.umc10th.domain.auth.dto.SignupReqDTO;
+import com.example.umc10th.domain.auth.exception.code.AuthSuccessCode;
+import com.example.umc10th.domain.auth.service.AuthService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    // 회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<String>> signup(
+            @RequestBody SignupReqDTO request
+    ) {
+
+        authService.signup(request);
+
+        return ResponseEntity.ok(
+                ApiResponse.onSuccess(AuthSuccessCode.SIGNUP_SUCCESS, null)
+        );
+    }
 }
